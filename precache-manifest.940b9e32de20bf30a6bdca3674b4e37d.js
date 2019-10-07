ea3109e362c238901f1a244c82f43ac8a46f4664
Shishir Gurung <gurungsh@gmail.com>
2019-10-07 00:31:33 -0500

    Updates

---
+++ precache-manifest.940b9e32de20bf30a6bdca3674b4e37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8c6d9c2cbe60690dc63afd1ce709db",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "895116c8016e6fe304be",
+    "url": "/react-carousel/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8a4590da21ac75f93c43",
+    "url": "/react-carousel/static/css/main.263e9293.chunk.css"
+  },
+  {
+    "revision": "895116c8016e6fe304be",
+    "url": "/react-carousel/static/js/2.e743fb61.chunk.js"
+  },
+  {
+    "revision": "8a4590da21ac75f93c43",
+    "url": "/react-carousel/static/js/main.c08cdd77.chunk.js"
+  },
+  {
+    "revision": "63643b250fa91b2cedf6",
+    "url": "/react-carousel/static/js/runtime-main.cffa5c3b.js"
+  }
+]);
